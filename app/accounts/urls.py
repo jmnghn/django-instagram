82ce29b1bc5cb283f78168917af5167094dac086
jngmnghn <jngmnghn@jngmnghnui-MacBookPro.local>
2020-02-26 20:02:39 +0900

    로그인, 회원가입 후 로그인, 로그아웃 구현

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('', views.temp, name='temp'),
 ]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,15 +1,32 @@
 from django.contrib import messages
+from django.contrib.auth import login as django_login
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 from django.shortcuts import render, redirect
 
 from .forms import SignupForm
+
+login = LoginView.as_view(template_name='accounts/login_form.html')
+
+
+# logout = LogoutView.as_view(next_page='/accounts/')
+
+
+def logout(request):
+    messages.success(request, '로그아웃')
+    return logout_then_login(request)
+
+
+def temp(request):
+    return render(request, 'accounts/temp.html')
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signuped_user = form.save()
             messages.success(request, '가입')
+            django_login(request, signuped_user)
             # signed_user.send_welcome_email()  # Celery
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
