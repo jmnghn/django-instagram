882d01a2e0fa82cfb08f591b2d5a36eff156418d
jngmnghn <jngmnghn@jngmnghnui-MacBookPro.local>
2020-02-26 18:53:45 +0900

    가입환영 메일 보내기

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 from django.db import models
+from django.template.loader import render_to_string
 
 
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
-    pass
 
-
+    # def send_welcome_email(self):
+    #     subject = "가입을 환영합니다."
+    #     content = render_to_string('accounts/welcome_email_content.txt', {
+    #         'user': self,
+    #     })
+    #     sender_email = "jeongmyeonghyeon@gmail.com"
+    #     send_mail(subject, content, sender_email, [self.email], fail_silently=False)
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -10,6 +10,7 @@ def signup(request):
         if form.is_valid():
             form.save()
             messages.success(request, '가입')
+            # signed_user.send_welcome_email()  # Celery
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
     else:
